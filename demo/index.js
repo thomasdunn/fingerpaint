51f630e2bab1da2b81bb0a652c066695718102c7
thomasdunn <thomas.dunn@gmail.com>
2015-03-15 04:16:29 +0000

    Updated demo with save/slideshow/etc.

--- demo/index.js
+++ demo/index.js
@@ -1,11 +1,12 @@
-/* global Hammer, FastClick */
+/* global Hammer, FastClick, PouchDB, blobUtil */
 
 (function() {
     var canvas,
         context,
         radius = 15,
-        color,
         pointerPositions = [],
+        db = new PouchDB('fingerpaint'),
+        debug = false,
         app = {
             'init': init
         };
@@ -17,6 +18,9 @@
         initTouch();
         initBrushPicker();
         initColorPicker();
+        initActions();
+        
+        PouchDB.debug.enable('*');
     }
     
     function initCanvas() {
@@ -27,13 +31,15 @@
         canvas.height = window.innerHeight;
 
         context.lineCap = context.lineJoin = 'round';
+
+        // clearDrawing();
     }
     
     function initTouch() {
         var mc = new Hammer(canvas, { });
 
         mc.on("tap", function(e) {
-            console.log('event type: ' + e.type);
+            log('event type: ' + e.type);
             
             // prevent the source event from doing it's native behavior
             e.preventDefault();
@@ -44,7 +50,7 @@
         });
         
         mc.on("panstart panend", function(e) {
-            console.log('event type: ' + e.type);
+            log('event type: ' + e.type);
             
             e.preventDefault();
 
@@ -57,7 +63,7 @@
         });
 
         mc.on("panleft panright panup pandown", function(e) {
-            console.log('event type: ' + e.type);
+            log('event type: ' + e.type);
             
             e.preventDefault();
 
@@ -113,6 +119,7 @@
     function initBrushPicker() {
         $('.brush').on('click', function() {
             radius = $(this).data('radius');
+            log('Brush size: ' + radius);
             $('.brush').removeClass('selected');
             $(this).addClass('selected');
         });
@@ -141,6 +148,15 @@
         $('#picker').show();
     }
     
+    function initActions() {
+        $('#save').on('click', function() {
+           saveCanvasImage();
+        });
+        $('#slideshow').on('click', function() {
+           openSlideshow();
+        });
+    }
+    
     //-----------Drawing functions 
     
     function changeColor(color) {
@@ -162,13 +178,67 @@
         context.lineWidth = radius * 2;
         context.stroke();
     }
+    
+    function clearDrawing() {
+        context.fillStyle = "#fff";
+        context.rect(0, 0, canvas.width, canvas.height);
+        context.fill();
+    }
+    
+    //-----------File Management
+    function saveCanvasImage() {
+        var id = (+(new Date)).toString(),
+            filename = 'image_' + id + '.png',
+            doc = {
+                _id: id,
+                _attachments: {}
+            };
+        
+        blobUtil.canvasToBlob(canvas).then(function (blob) {
+            doc._attachments[filename] = {
+                content_type: 'image/png',
+                data: blob
+            };
+            
+            // Cannot open the window from inside the promise
+            var openedWindow = window.open('', 'fingerpaint-slideshow');
+            
+            db.put(doc).then(function() {
+                // openImage(id, filename);
+                // openSlideshow();
+                openedWindow.location = 'slideshow.html';
+            }).catch(function(err) {
+                console.log(err);
+            });
+        }).catch(function (err) {
+            console.log(err);
+        });
+        
+        log('new doc id: ' + doc._id);
+    }
+
+    function openImage(id, filename) {
+        db.get(id, {attachments: true}).then(function(doc) {
+            var dataBase64 = doc._attachments[filename].data;
+            //log('Data: ' + dataBase64);
+            
+            window.open('data:image/png;base64,' + dataBase64, filename);
+        }).catch(function (err) {
+            console.log(err);
+        });
+    }
+    
+    function openSlideshow() {
+        window.open('slideshow.html', 'fingerpaint-slideshow');
+    }
+    
+    function log(msg) {
+        if (debug) {
+            console.log(msg);
+        }
+    }
 
 })().init();
-
-// Notes:
-// PouchDB for cross browser persistence with ability to sync to server
-//   http://pouchdb.com/getting-started.html
-//   http://pouchdb.com/api.html#save_attachment
 
 // TODO:
 //   Set content scalable meta for mobile
---
+++ demo/slideshow.js
@@ -0,0 +1,88 @@
+/* global PouchDB, FastClick, blueimp */
+
+(function() {
+    var db = new PouchDB('fingerpaint'),
+        debug = false,
+        app = {
+            'init': init
+        };
+
+    return app;
+    
+    function init() {
+        PouchDB.debug.enable('*');
+        
+        initTouch();
+        initImages();
+    }
+    
+    function initImages() {
+        var links = $('#links'),
+            imageBase64Data;
+
+        db.allDocs({
+          include_docs: true, 
+          attachments: true,
+          descending: true
+        }).then(function (result) {
+          result.rows.forEach(function(row) {
+            for (var filename in row.doc._attachments) {
+                imageBase64Data = row.doc._attachments[filename].data;
+
+                links.append(
+                    $('<a></a>')
+                        .attr('data-gallery', '')
+                        .attr('href', 'data:image/png;base64,' + imageBase64Data)
+                );
+            }
+            
+            initSlideshow();
+          });
+        }).catch(function (err) {
+          console.log(err);
+        });
+    }
+    
+    function initSlideshow() {
+        blueimp.Gallery($('#links a'), {
+            closeOnSwipeUpOrDown: true,
+            closeOnSlideClick: false,
+            closeOnEscape: false,
+            fullScreen: false,
+            useBootstrapModal: false,
+            onclose: function() { window.close(); }
+        });
+    }
+    
+    function initTouch() {
+        // disable context menu, often would appear after accidental 2nd finger touch
+        window.addEventListener('contextmenu', function (e) { // Not compatibile with IE < 9 but neither is canvas
+          e.preventDefault();
+        }, false);
+    
+        // Chrome: to disable right-swipe back browser, left-swipe forward browser
+        // set overscroll-history-navigation=disabled
+        // chrome://flags/#overscroll-history-navigation
+    
+        // This library is to remove the 300ms touch browser click delay
+        $(function() {
+          FastClick.attach(document.body);
+        });
+    }
+
+    // return base64 encoded image data from attachment filename from doc with id
+    function getImageData(id, filename) {
+        db.get(id, {attachments: true}).then(function(doc) {
+            return doc._attachments[filename].data;
+        }).catch(function (err) {
+            console.log(err);
+        });
+    }
+    
+    function log(msg) {
+        if (debug) {
+            console.log(msg);
+        }
+    }
+
+})().init();
