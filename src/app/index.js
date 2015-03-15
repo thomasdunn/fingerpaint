a8c9589ab16c8f617bddda3576809428a64e8a14
thomasdunn <thomas.dunn@gmail.com>
2015-03-15 05:30:31 +0000

    Ran generator-gulp-angular

---
+++ src/app/index.js
@@ -0,0 +1,10 @@
+'use strict';
+/*jshint esnext: true */
+
+import MainCtrl from './main/main.controller';
+import NavbarCtrl from '../components/navbar/navbar.controller';
+
+angular.module('fingerpaint', ['ngAnimate'])
+  .controller('MainCtrl', MainCtrl)
+  .controller('NavbarCtrl', NavbarCtrl)
+  ;
