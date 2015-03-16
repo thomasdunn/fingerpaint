d7cabb49f243193bb8acd3f674dc36bdfdd8a495
thomasdunn <thomas.dunn@gmail.com>
2015-03-16 02:16:21 +0000

    JSHint fixes

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -26,7 +26,7 @@ class MainCtrl {
     
     function initCanvas() {
         canvas = document.getElementById('canvas');
-        context = canvas.getContext("2d");
+        context = canvas.getContext('2d');
         
         canvas.width  = window.innerWidth;
         canvas.height = window.innerHeight;
@@ -39,7 +39,7 @@ class MainCtrl {
     function initTouch() {
         var mc = new Hammer(canvas, { });
 
-        mc.on("tap", function(e) {
+        mc.on('tap', function(e) {
             log('event type: ' + e.type);
             
             // prevent the source event from doing it's native behavior
@@ -50,20 +50,20 @@ class MainCtrl {
         	});
         });
         
-        mc.on("panstart panend", function(e) {
+        mc.on('panstart panend', function(e) {
             log('event type: ' + e.type);
             
             e.preventDefault();
 
         	e.pointers.forEach(function(pointer, index) {
-                if (e.type === "panend") {
+                if (e.type === 'panend') {
                     drawCircle(pointer.pageX, pointer.pageY);
                     pointerPositions[pointer.identifier] = undefined;
                 }
         	});
         });
 
-        mc.on("panleft panright panup pandown", function(e) {
+        mc.on('panleft panright panup pandown', function(e) {
             log('event type: ' + e.type);
             
             e.preventDefault();
@@ -127,22 +127,22 @@ class MainCtrl {
     }
     
     function initColorPicker() {
-        $("#picker").spectrum({
+        $('#picker').spectrum({
             change: changeColor,
             clickoutFiresChange: true, // keep selected color when change brush
             showPaletteOnly: true,
             showPalette:true,
             flat: true,
-            color: "#000",
+            color: '#000',
             palette: [
-                ["#000","#444","#666","#999","#ccc","#eee","#f3f3f3","#fff"],
-                ["#f00","#f90","#ff0","#0f0","#0ff","#00f","#90f","#f0f"],
-                ["#f4cccc","#fce5cd","#fff2cc","#d9ead3","#d0e0e3","#cfe2f3","#d9d2e9","#ead1dc"],
-                ["#ea9999","#f9cb9c","#ffe599","#b6d7a8","#a2c4c9","#9fc5e8","#b4a7d6","#d5a6bd"],
-                ["#e06666","#f6b26b","#ffd966","#93c47d","#76a5af","#6fa8dc","#8e7cc3","#c27ba0"],
-                ["#c00","#e69138","#f1c232","#6aa84f","#45818e","#3d85c6","#674ea7","#a64d79"],
-                ["#900","#b45f06","#bf9000","#38761d","#134f5c","#0b5394","#351c75","#741b47"],
-                ["#600","#783f04","#7f6000","#274e13","#0c343d","#073763","#20124d","#4c1130"]
+                ['#000','#444','#666','#999','#ccc','#eee','#f3f3f3','#fff'],
+                ['#f00','#f90','#ff0','#0f0','#0ff','#00f','#90f','#f0f'],
+                ['#f4cccc','#fce5cd','#fff2cc','#d9ead3','#d0e0e3','#cfe2f3','#d9d2e9','#ead1dc'],
+                ['#ea9999','#f9cb9c','#ffe599','#b6d7a8','#a2c4c9','#9fc5e8','#b4a7d6','#d5a6bd'],
+                ['#e06666','#f6b26b','#ffd966','#93c47d','#76a5af','#6fa8dc','#8e7cc3','#c27ba0'],
+                ['#c00','#e69138','#f1c232','#6aa84f','#45818e','#3d85c6','#674ea7','#a64d79'],
+                ['#900','#b45f06','#bf9000','#38761d','#134f5c','#0b5394','#351c75','#741b47'],
+                ['#600','#783f04','#7f6000','#274e13','#0c343d','#073763','#20124d','#4c1130']
             ]
         });
         
@@ -181,14 +181,14 @@ class MainCtrl {
     }
     
     function clearDrawing() {
-        context.fillStyle = "#fff";
+        context.fillStyle = '#fff';
         context.rect(0, 0, canvas.width, canvas.height);
         context.fill();
     }
     
     //-----------File Management
     function saveCanvasImage() {
-        var id = (+(new Date)).toString(),
+        var id = (+(new Date())).toString(),
             filename = 'image_' + id + '.png',
             doc = {
                 _id: id,
@@ -197,7 +197,9 @@ class MainCtrl {
         
         blobUtil.canvasToBlob(canvas).then(function (blob) {
             doc._attachments[filename] = {
+                /*jshint camelcase: false */
                 content_type: 'image/png',
+                /*jshint camelcase: true */
                 data: blob
             };
             
