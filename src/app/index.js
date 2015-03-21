ba422ebc246e0a314f4fc0a3b06927ba32b5dad5
thomasdunn <thomas.dunn@gmail.com>
2015-03-21 07:29:29 +0000

    Refactor color picker code to directive and use toolsService for pub/sub of selected color.

--- src/app/index.js
+++ src/app/index.js
@@ -5,6 +5,7 @@ import MainCtrl from './main/main.controller';
 import NavbarCtrl from '../components/navbar/navbar.controller';
 import BrushesController from '../components/brushes/brushes.controller';
 import fingerpaintBrushes from '../components/brushes/brushes.directive';
+import fingerpaintColors from '../components/colors/colors.directive';
 import ToolsService from '../components/tools/tools.service';
 
 angular.module('fingerpaint', ['ngAnimate'])
@@ -12,4 +13,5 @@ angular.module('fingerpaint', ['ngAnimate'])
   .controller('NavbarCtrl', NavbarCtrl)
   .controller('BrushesController', BrushesController)
   .directive('fingerpaintBrushes', fingerpaintBrushes)
+  .directive('fingerpaintColors', fingerpaintColors)
   .service('toolsService', ToolsService);
--- src/components/tools/tools.service.js
+++ src/components/tools/tools.service.js
@@ -3,19 +3,39 @@
 
 class ToolsService {
     
-    constructor() {
+    constructor($rootScope) {
+        this.$rootScope = $rootScope;
+
         this._brushRadius = 1;
+        this._paintColor = '#000000';
+        this._paintColorSetMessage = 'paintColorSetMessage';
+    }
+
+    // brush radius is read as from service as needed
+    get brushRadius() {
+        return this._brushRadius;
     }
 
     set brushRadius(radius) {
         this._brushRadius = radius;
     }
 
-    get brushRadius() {
+    // brush radius is read as from service as needed
+    get paintColor() {
         return this._brushRadius;
+    }
+
+    set paintColor(color) {
+        this.$rootScope.$broadcast(this._paintColorSetMessage, color);
+    }
+
+    onPaintColorsSet($scope, handler) {
+        $scope.$on(this._paintColorSetMessage, function(event, color) {
+            handler(color);
+        });
     }
 }
 
-ToolsService.$inject = [];
+ToolsService.$inject = ['$rootScope'];
 
 export default ToolsService;
